59aad341a3bd9dfc7ef104e78af06af03accb258
Peter Mamonov <pmamonov@gmail.com>
2015-02-05 01:18:55 +0300

    idump.py: measure delay between received data packets

--- idump.py
+++ idump.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import sys, serial, struct
+import sys, serial, struct, time
 
 if len(sys.argv) < 2:
 	print """
@@ -10,9 +10,13 @@ USAGE:
 
 sr = serial.Serial(sys.argv[1], 38400)
 
+t = time.time()
+
 while 1:
 	r = struct.unpack("<BBH", sr.read(4))
 	if r[0] == 0xaa:
-		print "%d %d %d" % r
+		t1 = time.time()
+		print "%s %6.3f %d %d %d" % tuple([time.strftime("%Y-%m-%d %H:%M:%S"), t1 - t] + list(r))
+		t = t1
 	else:
 		sr.read(1)
